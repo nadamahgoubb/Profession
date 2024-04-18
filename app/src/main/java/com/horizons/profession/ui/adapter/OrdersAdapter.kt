c1ff537901c848787bde7eb1e1f7c261ccc92da7
nada <nada.moawaad@gmail.com>
2024-04-18 15:39:57 +0200

    EDIT - edit version code

--- app/src/main/java/com/horizons/profession/ui/adapter/OrdersAdapter.kt
+++ app/src/main/java/com/horizons/profession/ui/adapter/OrdersAdapter.kt
@@ -33,9 +33,6 @@ class OrdersAdapter(
     override fun onBindViewHolder(holder: OrdersViewHolder, position: Int) {
         var currentItem = list.get(position)
         holder.binding.tvOrderId.text = "# " + currentItem.orderId
-      //   holder.binding.tvTime.text =
-        //     getDateTime(    )+ " "
-    //   SimpleDateFormat("HH:mm:ss", Locale.ENGLISH).format(  currentItem.orderTime.toString().toDateTime("HH:mm:ss"))+ currentItem.orderDate.toString()
 
             holder.binding.tvTime.text = toTwelevePattern(currentItem.orderTime.toString()).toString()+ " "+currentItem.orderDate.toString()
 
@@ -58,8 +55,10 @@ class OrdersAdapter(
             LayoutInflater.from(parent.context), parent, false
         )
         if (state == Constants.PREV_ORDER) _binding?.tvStatusCompelted?.isVisible = true
-        else if (state == Constants.New_ORDER) _binding?.tvStatusMsgWaitingApproval?.isVisible =
-            true
+        else if (state == Constants.New_ORDER) {
+            _binding?.tvStatusMsgWaitingApproval?.isVisible = true
+            _binding?.lytCall?.isVisible= false
+        }
         else if (state == Constants.CURRENT_ORDER) _binding?.tvStatusInProgress?.isVisible = true
         return OrdersViewHolder(_binding!!)
     }
